3fa4015eac08cfc6eb8401b78782688fcb672be7
Topicesst <topicesstofficial@gmail.com>
2024-03-18 20:58:03 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,67 +1,25 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
-import { useTelegram } from "./hooks/useTelegram";
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import { Route, Routes } from 'react-router-dom';
-import Card from "./components/Card/Card";
-import Cart from "./components/Cart/Cart";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
-import { getData } from "./db/db"; // Імпортуємо функцію getData для отримання даних про їжу
 
 function App() {
-    const { onToggleButton, tg } = useTelegram();
-    const [cartItems, setCartItems] = useState([]); // Стан для елементів кошика
-    const foods = getData(); // Отримуємо дані про їжу
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, [tg]); // Викликаємо телеграм тільки при зміні tg
-
-    // Функція додавання продукту до кошика
-    const onAdd = (food) => {
-        const exist = cartItems.find((x) => x.id === food.id);
-        if (exist) {
-            setCartItems(
-                cartItems.map((x) =>
-                    x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
-                )
-            );
-        } else {
-            setCartItems([...cartItems, { ...food, quantity: 1 }]);
-        }
-    };
-
-    // Функція видалення продукту з кошика
-    const onRemove = (food) => {
-        const exist = cartItems.find((x) => x.id === food.id);
-        if (exist.quantity === 1) {
-            setCartItems(cartItems.filter((x) => x.id !== food.id));
-        } else {
-            setCartItems(
-                cartItems.map((x) =>
-                    x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
-                )
-            );
-        }
-    };
-
-    // Функція оформлення замовлення
-    const onCheckout = () => {
-        // Ваша логіка для оформлення замовлення
-    };
+    }, [])
 
     return (
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<Cart cartItems={cartItems} onCheckout={onCheckout} />} />
-                <Route path="/form" element={<Form />} />
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
             </Routes>
-            <div className="cards__container">
-                {foods.map((food) => (
-                    <Card key={food.id} food={food} onAdd={onAdd} onRemove={onRemove} />
-                ))}
-            </div>
         </div>
     );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,19 +1,10 @@
-import React from "react";
-import "./Button.css";
+import React from 'react';
+import './Button.css';
 
-// Компонент кнопки, який приймає тип, заголовок, стан відключення та обробник кліків
-function Button({ type, title, disable, onClick }) {
+const Button = (props) => {
     return (
-        <button
-            className={`btn ${
-                type === "add" ? "add" : type === "remove" ? "remove" : type === "checkout" ? "checkout" : ""
-            }`}
-            disabled={disable} // Встановлює стан відключення кнопки
-            onClick={onClick} // Встановлює обробник кліків на кнопці
-        >
-            {title} {/* Відображає заголовок кнопки */}
-        </button>
+        <button {...props} className={'button ' + props.className}/>
     );
-}
+};
 
-export default Button; // Експортує компонент Button
+export default Button;
