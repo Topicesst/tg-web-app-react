682b7cc282b21dec0eacb8277944ec79ada9a124
Topicesst <topicesstofficial@gmail.com>
2024-03-19 20:56:28 +0200

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Button from "../Button/Button";
 import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css';
 
 const Header = () => {
     const { user, onClose } = useTelegram();
+    const location = useLocation();
+
+    // Перевірка, чи ми на головній сторінці
+    const isHomePage = location.pathname === '/';
+
+    // Відображення заголовка тільки на головній сторінці
+    if (!isHomePage) {
+        return null; // Якщо не на головній сторінці, повертаємо null, щоб не відображати Header
+    }
 
     return (
         <div className={'header'}>
