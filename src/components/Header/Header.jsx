6bc42e75ded35a9fe29e8d819c2126bfbd981711
Topicesst <topicesstofficial@gmail.com>
2024-03-19 19:10:32 +0200

    init

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Button from "../Button/Button";
-import {useTelegram} from "../../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 import './Header.css';
 
-const Header = () => {
-    const {user, onClose} = useTelegram();
+const Header = ({ title }) => { // Додаємо параметр title
+    const { user, onClose } = useTelegram();
 
     return (
         <div className={'header'}>
+            <h1 className="title">{title}</h1> {"Замовлення їжі:"}
             <Button onClick={onClose}></Button>
-            <span className={'username'}>
-                {user?.username}
-            </span>
         </div>
     );
 };
