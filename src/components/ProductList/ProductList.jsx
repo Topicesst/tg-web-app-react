0d2a83214ce918918f27f9d2f20ea86398fe1cbc
Topicesst <132078398+Topicesst@users.noreply.github.com>
2024-03-28 18:34:20 +0200

    ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -34,20 +34,9 @@ const ProductList = () => {
     const [addedItems, setAddedItems] = useState([]);
     const {tg, queryId} = useTelegram();
 
-    const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-            queryId,
-        }
-        fetch('http://80.85.143.220:8000/web-data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-    }, [addedItems])
+const onSendData = ()=>{
+    console.log('PRESS BUTTON');
+}
 
    useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
